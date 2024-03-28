d70f8170a4cf8fc34fbb15450d8f2e1a6ad2b1f7
hitendra <hitendra369432@gmail.com>
2024-03-28 15:08:22 +0530

    fixed username

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,6 +4,11 @@ import bcrypt from 'bcrypt';
 
 // Define the user schema
 const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        trim:true,
+    },
     email: {
         type: String,
         required: true,
